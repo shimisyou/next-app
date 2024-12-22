319ed48116211e6bab05f9ef389961f341836797
shimizu <shimisyoudev@gmail.com>
2024-12-22 11:36:22 +0900

    ci調整

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   images: {
     domains: ['placehold.jp'], // 使用するホスト名を追加
   },

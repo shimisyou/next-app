7e7086e43311975cbfb335da0102fa9ed24f443f
shimizu <shimisyoudev@gmail.com>
2024-12-30 00:20:26 +0900

    リンク修正

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from 'next';
 
+const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'yourdomain.com', 'shimisyou.github.io'], // 必要なドメインを追加
     unoptimized: true,
   },
-  output: 'export', // 静的エクスポートを有効にする
-  basePath: '/next-app/next', // 公開URLに合わせる
-  assetPrefix: '/next-app/next', // 静的ファイルの基準パスも一致させる
+  output: 'export',
+  basePath: isProduction ? '/next-app/next' : '',
+  assetPrefix: isProduction ? '/next-app/next' : '',
 };
 
 export default nextConfig;

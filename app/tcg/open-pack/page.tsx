91f10f849ea0f98ebf651f130cdd0b99edf6d01d
shimizu <shimisyoudev@gmail.com>
2025-01-01 00:07:12 +0900

    クライアント側でmockデータ生成

--- app/tcg/open-pack/page.tsx
+++ app/tcg/open-pack/page.tsx
@@ -1,14 +1,24 @@
+'use client';
+
 import NeumorphicButton from '@/components/elements/Button/NeumorphicButton/NeumorphicButton';
 import { PackSwiperContainer } from '@/features/TCG/components/PackSwiper/PackSwiperContainer';
 import { generateMockPacksWithCacheBuster } from '@/features/TCG/mocks/packs';
+import { Pack } from '@/features/TCG/types';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-const fetchPacks = async () => {
-  return Promise.resolve(generateMockPacksWithCacheBuster());
-};
+const OpenPackPage = () => {
+  const [packs, setPacks] = useState<Pack[]>([]);
 
-const OpenPackPage = async () => {
-  const packs = await fetchPacks();
+  useEffect(() => {
+    // クライアントサイドでモックデータを生成
+    const generatedPacks = generateMockPacksWithCacheBuster();
+    setPacks(generatedPacks);
+  }, []);
+
+  if (!packs.length) {
+    return <div>Loading packs...</div>;
+  }
 
   return (
     <>

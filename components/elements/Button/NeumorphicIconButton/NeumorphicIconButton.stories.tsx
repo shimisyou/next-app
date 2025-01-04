1abc8eeb3764de956c779282f42569ba6d8280ce
shimizu <shimisyoudev@gmail.com>
2025-01-04 12:54:01 +0900

    テスト修正

--- components/elements/Button/NeumorphicIconButton/NeumorphicIconButton.stories.tsx
+++ components/elements/Button/NeumorphicIconButton/NeumorphicIconButton.stories.tsx
@@ -6,6 +6,9 @@ import NeumorphicIconButton from './NeumorphicIconButton';
 const meta = {
   component: NeumorphicIconButton,
   tags: ['autodocs'],
+  args: {
+    icon: <HomeIcon sx={{ color: '#444', fontSize: { xs: 28, md: 36 } }} />,
+  },
 } satisfies Meta<typeof NeumorphicIconButton>;
 
 export default meta;
@@ -15,13 +18,11 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     isInset: false,
-    children: <HomeIcon sx={{ color: '#444', fontSize: { xs: 28, md: 36 } }} />,
   },
 };
 
 export const Inset: Story = {
   args: {
     isInset: true,
-    children: <HomeIcon sx={{ color: '#444', fontSize: { xs: 28, md: 36 } }} />,
   },
 };
